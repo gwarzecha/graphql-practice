0a87285f756a616f2ecefa2a97b9bb4ab0d92254
gwarz <gmwarzecha@tutanota.com>
2021-07-10 15:32:45 -0700

    added the Form component back to PostForm and reconfigured NewPost component

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -4,62 +4,16 @@ import { Mutation } from "react-apollo";
 // takes query strings and converts them into something we can use
 import gql from "graphql-tag";
 
+// this is essentially a reusable form component that could be duplicated
 export default class NewPost extends Component {
-  state = {
-    title: "",
-    body: "",
-  };
-
-  handleInput = (e) => {
-    const formData = {};
-    formData[e.target.name] = e.target.value;
-    this.setState({ ...formData });
-  };
-
   render() {
-    const { title, body } = this.state;
     return (
       <div>
         <h1>New Post</h1>
-        {/* <PostForm /> */}
-        <Mutation
-          mutation={NEW_POST}
-          variables={{
-            title,
-            body,
-          }}
-        >
-          { createPost  => (
-            <form
-              onSubmit={(e) => {
-                // prevents form from refreshing the page
-                e.preventDefault();
-                createPost()
-                  .then(() => {
-                    this.setState({
-                      title: "",
-                      body: "",
-                    });
-                  })
-                  .catch((e) => console.log(e));
-              }}
-            >
-              <input
-                name="title"
-                type="text"
-                onChange={this.handleInput}
-                value={title}
-                placeholder="title"
-              />
-              <textarea
-                name="body"
-                type="text"
-                onChange={this.handleInput}
-                value={body}
-                placeholder="body"
-              />
-              <button>Submit</button>
-            </form>
+        <Mutation mutation={NEW_POST}>
+          {createPost => (
+            // passes the createPost function down into the form component
+            <PostForm onSubmit={createPost} />
           )}
         </Mutation>
       </div>
